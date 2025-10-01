596e33b0b565cb42974124beabea968e4d93f9aa
Mohamed Derouiche <mohamed.derouiche@etudiant-enit.utm.tn>
2025-10-01 17:05:27 +0100

    Add_character_network

--- gradio_app.py
+++ gradio_app.py
@@ -1,14 +1,10 @@
 import gradio as gr
-
-import sys
-sys.path.append("..")  # Go up one folder
 from theme_classifier import ThemeClassifier
+from character_network import NamedEntityRecognizer, CharacterNetworkGenerator
+
 import os
 from dotenv import load_dotenv
-
 load_dotenv()
-
-
 
 def get_themes(theme_list_str,subtitles_path,save_path):
     theme_list = theme_list_str.split(',')
@@ -17,8 +13,8 @@ def get_themes(theme_list_str,subtitles_path,save_path):
 
     # Remove dialogue from the theme list
     theme_list = [theme for theme in theme_list if theme != 'dialogue']
-    existing_themes = [t for t in theme_list if t in output_df.columns]
-    output_df = output_df[existing_themes]
+    output_df = output_df[theme_list]
+
     output_df = output_df[theme_list].sum().reset_index()
     output_df.columns = ['Theme','Score']
 
@@ -35,8 +31,15 @@ def get_themes(theme_list_str,subtitles_path,save_path):
 
     return output_chart
 
+def get_character_network(subtitles_path,ner_path):
+    ner = NamedEntityRecognizer()
+    ner_df = ner.get_ners(subtitles_path,ner_path)
 
+    character_network_generator = CharacterNetworkGenerator()
+    relationship_df = character_network_generator.generate_character_network(ner_df)
+    html = character_network_generator.draw_network_graph(relationship_df)
 
+    return html
 
 def main():
     with gr.Blocks() as iface:
@@ -53,11 +56,23 @@ def main():
                         save_path = gr.Textbox(label="Save Path")
                         get_themes_button =gr.Button("Get Themes")
                         get_themes_button.click(get_themes, inputs=[theme_list,subtitles_path,save_path], outputs=[plot])
+
+        # Character Network Section
+        with gr.Row():
+            with gr.Column():
+                gr.HTML("<h1>Character Network (NERs and Graphs)</h1>")
+                with gr.Row():
+                    with gr.Column():
+                        network_html = gr.HTML()
+                    with gr.Column():
+                        subtitles_path = gr.Textbox(label="Subtutles or Script Path")
+                        ner_path = gr.Textbox(label="NERs save path")
+                        get_network_graph_button = gr.Button("Get Character Network")
+                        get_network_graph_button.click(get_character_network, inputs=[subtitles_path,ner_path], outputs=[network_html])
+
+
     iface.launch(share=True, debug=True)
+            
 
-
-
-
-
-if __name__ == "__main__":
-    main()  
+if __name__ == '__main__':
+    main()
