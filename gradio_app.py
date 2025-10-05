f34cf6959cd9199fc8ba142cf4bfa4484c72ab20
Mohamed Derouiche <mohamed.derouiche@etudiant-enit.utm.tn>
2025-10-05 19:24:59 +0100

    Addedapp_derbug

--- gradio_app.py
+++ gradio_app.py
@@ -74,17 +74,13 @@ def classify_text(text_classifcation_model,text_classifcation_data_path,text_to_
     return output
 
 
-
+character_chatbot = CharacterChatBot("AbdullahTarek/Naruto_Llama-3-8B",
+                                      huggingface_token=os.getenv('huggingface_token'))
 
 def chat_with_character_chatbot(message, history):
-    character_chatbot = CharacterChatBot(
-        "AbdullahTarek/Naruto_Llama-3-8B",  # fine‑tuned model on HF Hub
-        huggingface_token=os.getenv("huggingface_token")
-    )
-
     output = character_chatbot.chat(message, history)
-    output = output["content"].strip()
-    return output
+    return output['content'].strip()
+
 
 
 def main():
